32c7f8ec01ea8609a31a932a989e080c0f60b6dc
Krasimir Nikolov <wirert@gmail.com>
2023-09-14 19:02:17 +0300

    03. queue

--- 01. Linear Data Structures - Lab/Problem03.Queue/Queue.cs
+++ 01. Linear Data Structures - Lab/Problem03.Queue/Queue.cs
@@ -8,41 +8,82 @@
     {
         private class Node
         {
-            
+            public T Item { get; set; }
+
+            public Node Next { get; set; }
         }
 
         private Node head;
 
-        public int Count => throw new NotImplementedException();
+        public int Count { get; private set; }
 
         public void Enqueue(T item)
         {
-            throw new NotImplementedException();
+            var node = new Node() { Item = item };
+
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                var tail = head;
+                while (tail.Next != null)
+                {
+                    tail = tail.Next;
+                }
+
+                tail.Next = node;
+            }
+
+            Count++;
         }
 
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            var result = Peek();
+            var newHead = head.Next;
+            head.Next = null;
+            head = newHead;
+            Count--;
+
+            return result;
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("The queue is empty!");
+            }
+
+            return head.Item;
         }
 
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
+            foreach (var member in this)
+            {
+                if (member.Equals(item))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var node = head;
+            while (node != null)
+            {
+                yield return node.Item;
+                node = node.Next;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
+            => GetEnumerator();
     }
 }
