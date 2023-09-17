95d36612591755820c956d12b535a4b4402894ce
Krasimir Nikolov <wirert@gmail.com>
2023-09-17 18:19:06 +0300

    07.Sequence N,M

---
+++ 01.1. Linear-Data-Structures-Exercise/05.ReverseNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _05.ReverseNumbers
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var stack = new Stack<int>();
+            for (int i = 0; i < n; i++)
+            {
+                stack.Push(int.Parse(Console.ReadLine()));
+            }
+
+            Console.WriteLine(string.Join(" ", stack));
+        }
+    }
+}
