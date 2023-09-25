a7bf7ea2740027215bbb47dd8781429cc25eb190
Krasimir Nikolov <wirert@gmail.com>
2023-09-25 17:31:07 +0300

    03.1. Binary Tree

---
+++ 03.Heaps and Binary Trees - Lab/Demo/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Demo
+{
+    using _01.BinaryTree;
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new BinaryTree<int>(17,
+                            new BinaryTree<int>(9, 
+                                    new BinaryTree<int>(3, null, null),
+                                    new BinaryTree<int>(11, null, null)),
+                            new BinaryTree<int>(25, 
+                                    new BinaryTree<int>(20, null, null),
+                                    new BinaryTree<int>(31, null, null))
+            );
+
+
+        }
+    }
+}
