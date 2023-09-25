b5f9f60320eff9fd328f73c5c647710ccedb13ae
Krasimir Nikolov <wirert@gmail.com>
2023-09-25 18:12:00 +0300

    03.2 MaxHeap

--- 03.Heaps and Binary Trees - Lab/02.MaxHeap/MaxHeap.cs
+++ 03.Heaps and Binary Trees - Lab/02.MaxHeap/MaxHeap.cs
@@ -1,20 +1,43 @@
 ﻿namespace _02.MaxHeap
 {
     using System;
+    using System.Collections.Generic;
 
     public class MaxHeap<T> : IAbstractHeap<T>
         where T : IComparable<T>
     {
-        public int Size { get { throw new NotImplementedException(); } }
+        private List<T> heap;
+
+        public MaxHeap()
+        {
+            heap = new List<T>();
+        }
+
+        public int Size => heap.Count;
 
         public void Add(T element)
         {
-            throw new NotImplementedException();
+            heap.Add(element);
+            Heapify(Size - 1);
+        }
+       
+        public T Peek() => heap[0];
+
+        private void Heapify(int index)
+        {
+            if (index == 0) return;
+
+            var parentIndex = (index - 1) / 2;
+
+            if (heap[parentIndex].CompareTo(heap[index]) == -1)
+            {
+                var copy = heap[parentIndex];
+                heap[parentIndex] = heap[index];
+                heap[index] = copy;
+
+                Heapify(parentIndex);
+            }
         }
 
-        public T Peek()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
