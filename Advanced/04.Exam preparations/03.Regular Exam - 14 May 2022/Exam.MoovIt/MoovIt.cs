d5a5d125cb7e50f238dcad396a6d597d98191103
Krasimir Nikolov <wirert@gmail.com>
2023-11-28 18:49:44 +0200

    03.Regular Exam - 14 May 2022: 02. MoovIt

--- Advanced/04.Exam preparations/03.Regular Exam - 14 May 2022/Exam.MoovIt/MoovIt.cs
+++ Advanced/04.Exam preparations/03.Regular Exam - 14 May 2022/Exam.MoovIt/MoovIt.cs
@@ -1,50 +1,99 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Exam.MoovIt
 {
     public class MoovIt : IMoovIt
     {
-        public int Count => throw new NotImplementedException();
+        private Dictionary<string, Route> routesById = new Dictionary<string, Route>();
+        private Dictionary<string, Route> routesByDistAndLocation = new Dictionary<string, Route>();
+
+        public int Count => routesById.Count;
+
+        public bool Contains(Route route)
+        {
+            return routesById.ContainsKey(route.Id) || routesByDistAndLocation.ContainsKey(GetKey((route)));
+        }
 
         public void AddRoute(Route route)
         {
-            throw new NotImplementedException();
+            if (Contains(route))
+            {
+                throw new ArgumentException();
+            }
+
+            routesById.Add(route.Id, route);
+            routesByDistAndLocation.Add(GetKey(route), route);
         }
 
         public void ChooseRoute(string routeId)
         {
-            throw new NotImplementedException();
-        }
+            if (!routesById.ContainsKey(routeId))
+            {
+                throw new ArgumentException();
+            }
 
-        public bool Contains(Route route)
-        {
-            throw new NotImplementedException();
+            routesById[routeId].Popularity++;
         }
 
         public IEnumerable<Route> GetFavoriteRoutes(string destinationPoint)
-        {
-            throw new NotImplementedException();
-        }
+            => routesById.Values
+                .Where(r => r.IsFavorite)
+                .Where(r => r.LocationPoints.Contains(destinationPoint) && 
+                            r.LocationPoints[0] != destinationPoint)
+                .OrderBy(r => r.Distance)
+                .ThenByDescending(r => r.Popularity);        
 
         public Route GetRoute(string routeId)
         {
-            throw new NotImplementedException();
+            if (!routesById.ContainsKey(routeId))
+            {
+                throw new ArgumentException();
+            }
+
+            return routesById[routeId];
         }
 
         public IEnumerable<Route> GetTop5RoutesByPopularityThenByDistanceThenByCountOfLocationPoints()
-        {
-            throw new NotImplementedException();
-        }
+            => routesById.Values
+            .OrderByDescending(r => r.Popularity)
+            .ThenBy(r => r.Distance)
+            .ThenBy(r => r.LocationPoints.Count)
+            .Take(5);
 
         public void RemoveRoute(string routeId)
         {
-            throw new NotImplementedException();
+            if (!routesById.ContainsKey(routeId))
+            {
+                throw new ArgumentException();
+            }
+            var route = routesById[routeId];
+            routesById.Remove(routeId);
+            routesByDistAndLocation.Remove(GetKey(route));
         }
 
         public IEnumerable<Route> SearchRoutes(string startPoint, string endPoint)
+            => routesById.Values
+                .Where(r => 
+                {
+                    var startIndex = r.LocationPoints.IndexOf(startPoint);
+                    var endIndex = r.LocationPoints.IndexOf(endPoint);
+
+                    return startIndex >= 0 && endIndex > startIndex;
+                })
+                .OrderByDescending(r => r.IsFavorite)
+                .ThenBy(r =>
+                {
+                    var startIndex = r.LocationPoints.IndexOf(startPoint);
+                    var endIndex = r.LocationPoints.IndexOf(endPoint);
+                    return endIndex - startIndex;
+                })
+                .ThenByDescending(r => r.Popularity);        
+
+        private string GetKey(Route route)
         {
-            throw new NotImplementedException();
+            return route.Distance.ToString() + route.LocationPoints[0] + route.LocationPoints[route.LocationPoints.Count - 1];
         }
     }
 }
--- Advanced/04.Exam preparations/03.Regular Exam - 14 May 2022/Exam.MoovIt/Route.cs
+++ Advanced/04.Exam preparations/03.Regular Exam - 14 May 2022/Exam.MoovIt/Route.cs
@@ -4,6 +4,15 @@ namespace Exam.MoovIt
 {
     public class Route
     {
+        public Route(string id, double distance, int popularity, bool isFavorite, List<string> locationPoints)
+        {
+            this.Id = id;
+            this.Distance = distance;
+            this.Popularity = popularity;
+            this.IsFavorite = isFavorite;
+            this.LocationPoints = locationPoints;
+        }
+
         public string Id { get; set; }
 
         public double Distance { get; set; }
@@ -13,14 +22,5 @@ namespace Exam.MoovIt
         public bool IsFavorite { get; set; }
 
         public List<string> LocationPoints { get; set; } = new List<string>();
-
-        public Route(string id, double distance, int popularity, bool isFavorite, List<string> locationPoints)
-        {
-            this.Id = id;
-            this.Distance = distance;
-            this.Popularity = popularity;
-            this.IsFavorite = isFavorite;
-            this.LocationPoints = locationPoints;
-        }
     }
 }
